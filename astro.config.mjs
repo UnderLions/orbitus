4bf5bacb88f8f7021f1017838a43a8d71c6ff3a3
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-06-30 21:08:06 +0530

    npm init

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+
+export default defineConfig({
+  srcDir : "web/src",
+  publicDir : "web/public",
+  outDir : "web/build",
+  cacheDir : "web/cache",
+
+  output : "static",
+  compressHTML : true,
+
+	integrations: [tailwind()],
+
+  build : {
+    // directory based routing
+    // pages/index.astro      -> /
+    // pages/post/index.astro -> /post
+    format : "directory",
+    assets : "__assets",
+    redirects : true,
+    inlineStylesheets : 'auto',
+
+  },
+})
