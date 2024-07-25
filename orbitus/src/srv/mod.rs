1df8a7db77ee7ff3ecb16241013545229f5d0e07
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-07-26 02:47:23 +0530

    added rt

--- orbitus/src/srv/mod.rs
+++ orbitus/src/srv/mod.rs
@@ -1,19 +1,40 @@
 use std::usize;
 
 use crate::cli::StartCommandOptions;
-use axum;
+use axum::{self, routing::future::RouteFuture};
 use tokio::runtime::{Builder, Runtime};
+use axum::Router;
+use axum::response::IntoResponse;
+use axum::routing::get;
 
+fn router() -> Router<> {
+    Router::new()
+        .route("/", get(|| async { "<p>404</p>".into_response() }))
+}
 
 fn multi_thread_runtime(n_threads: usize) -> Result<Runtime, std::io::Error> {
     Builder::new_multi_thread()
         .worker_threads(n_threads)
-        .enable_io()
+        .enable_all()
         .thread_name("orbitus_rt")
+        .on_thread_stop(|| {
+            println!("stopping thread");
+        })
+        .on_thread_start(|| {
+            println!("starting thread");
+        })
         .build()
 }
 
-
-pub fn start_serv(StartCommandOptions { host ,..}: StartCommandOptions) {
+pub fn start_serv(StartCommandOptions { host, .. }: StartCommandOptions) {
     println!("host at {:?}", host);
+    let runnerer = multi_thread_runtime(2).unwrap();
+    let _block_on = runnerer.block_on(
+        async {
+            let tcp = tokio::net::TcpListener::bind(host).await.unwrap();
+            let routes = router();
+            let _ = axum::serve(tcp, routes).await;
+
+        }
+    );
 }
