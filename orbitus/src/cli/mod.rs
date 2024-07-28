adfd4b715c5b5f9e6253da9c8a6853d1db45b246
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-07-28 21:20:52 +0530

    added some documentations

--- orbitus/src/cli/mod.rs
+++ orbitus/src/cli/mod.rs
@@ -2,40 +2,48 @@ use crate::srv;
 use clap::{
     command, Args, Parser, Subcommand
 };
-
 use std::net::SocketAddr;
-
 
 
 #[derive(Debug ,Parser)]
 #[command(about,long_about)]
 struct Cli {
+
     #[command(subcommand)]
     cmd: CliCommands
 }
 
 
-
+// TODO : add environment variables to override default option
 #[derive(Debug,Subcommand)]
 pub enum CliCommands{
+    // subcommand to start listening
+    // <bin> start --options
     Start(StartCommandOptions)
 }
 
 
 #[derive(Debug, Args)]
 pub struct StartCommandOptions {
+
+    // tcp socket address for initiate
     #[arg(long="host",help="specify ip address and port", default_value="127.0.0.1:8080")]
     pub host: SocketAddr
+    // TODO : add n_threads arg for async runtime
 }
 
-// #[allow(non_snake_case)]
+
 pub fn init() {
+    // initialiate cli parser
     let cli = Cli::parse();
 
     match cli.cmd {
         CliCommands::Start(start_command_options) => {
+            // start service have tokio runtime bulder and axum service
+            // axum router on srv::route module
             srv::start_serv(start_command_options);
         }
+        // no default_value currently
         // _ => {}
     }
 }
--- orbitus/src/main.rs
+++ orbitus/src/main.rs
@@ -1,9 +1,28 @@
+// TODO : development lints here some times unused and/or dead_code allowed
 // #![allow(unused)]
-#![forbid(unsafe_code)]
+//
+//
+//
+// --------------------------------------------------------------------------
+
+
+// LINTS
+//
+// #![forbid(unsafe_code)]
+// it is not necessary ,because already done in Cargo.toml
+// so no unsafe_code here!
+// 
+// TODO: Doc it
+// #![warn(missing_docs)]
+
+
+
+// include all modules in this crate
 mod cli;
 mod srv;
 
 
 fn main() {
+    // call cli initializer
     cli::init();
 }
