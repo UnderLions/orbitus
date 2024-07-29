b970bf26c4c338abdf500fc43fe5e77029352f4d
0x1F8F <86254906+0x1F8F@users.noreply.github.com>
2024-07-29 14:29:03 +0530

    update router

--- orbitus/src/srv/routs.rs
+++ orbitus/src/srv/routs.rs
@@ -1,8 +1,13 @@
-use axum::response::IntoResponse;
 use axum::Router;
-
+use axum::response::IntoResponse;
+use axum::extract::Path;
+use axum::routing::get;
+use axum::http::header;
+use tracing::{info, warn};
 use crate::err::Exception;
+use rust_embed::Embed;
 
+use super::pages::main_page_handeler_get;
 
 pub fn fallback_route() -> Router {
     Router::new()
@@ -10,3 +15,35 @@ pub fn fallback_route() -> Router {
             Exception::ClientSideError404
         })
 }
+
+pub async fn asset_loader(Path(path) : Path<String>) -> impl IntoResponse {
+    info!("Asset loader: {}",path.clone());
+    match Asset::get(&path.clone()) {
+        Some(content) => {
+            let mime = mime_guess::from_path(path).first_or_octet_stream();
+            info!("Dispatch Type : {}",mime);
+            ([(header::CONTENT_TYPE,mime.as_ref())],content.data).into_response()
+        },
+        None => {
+            warn!("invalid resource requested");
+            ("ASSET NOT FOUND").into_response()
+        }
+    }
+}
+
+pub fn spa_router() -> Router {
+    Router::new()
+        .route("/", get(main_page_handeler_get))
+        .merge(fallback_route())
+}
+
+pub fn asset_router() -> Router {
+    Router::new()
+        .route_service("/__assets/*path", get(asset_loader))
+}
+
+#[derive(Embed)]
+#[folder = "src/build/__assets/"]
+struct Asset;
+
+
